5e34a9629daf1769810c2818f2c5d2d5b2f0a96e
dukevaishnav1000 <dukevaishnav3853@gmail.com>
2022-08-14 17:13:27 +0530

    pushing all of them

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
+import Coin from './Coin.js';
+
 
 function App() {
+
+  const [coins,setCoins] =useState([]);
+  const [search,setSearch]=useState('');
+
+  useEffect(()=>{
+
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res=>{
+      setCoins(res.data)
+    }).catch(error=>console.log("error  while fetching data from api error"+error));
+
+    console.log(coins);
+  },[])
+
+  const handleChange=(e)=>{
+    setSearch(e.target.value);
+  }
+
+  const filteredCoins=coins.filter(coin=>{
+   return coin.name.toLowerCase().includes(search.toLowerCase());
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+    <div className="coin-search">
+      <h1 className="coin-text">Crypto Tracker</h1>
+      <form>
+        <input type="text" placeholder='search... ' className="coin-input" onChange={handleChange}/>
+      </form>
+    </div>
+    {filteredCoins.map(coin=>{
+      return <Coin key={coin.id} name={coin.name} image={coin.image} price={coin.current_price}  symbol={coin.symbol} volume={coin.total_volume} priceChange={coin.price_change_percentage_24h} marketcap={coin.market_cap}/>
+    })}
     </div>
   );
 }
